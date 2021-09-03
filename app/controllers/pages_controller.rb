deb80160b6619c47e99e8e2f4db6da3047eac341
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-03 12:05:52 +0900

    created route for Cart inside pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :cart ]
 
   def home
   end
 
   def about
+  end
+
+  def cart
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get 'about', to: 'pages#about'
+  get 'cart', to: 'pages#cart'
   resources :products, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
