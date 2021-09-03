7b94b2b40368df8daf0bc5c61a3f33ce8188fde3
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-03 17:06:51 +0900

    created Simple Form for review and only shown if you are logged in. if not redirect to log in

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,5 +8,7 @@ class ProductsController < ApplicationController
     def show
         @product = Product.find(params[:id])
         @reviews = @product.reviews
+        @review = Review.new
+        @user = current_user
     end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,7 @@ class ReviewsController < ApplicationController
     def new
     end
     def create
+        
     end
     def edit
     end
