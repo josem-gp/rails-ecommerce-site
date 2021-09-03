7a81d99513e06a3a3e14affa966fddc4f79257f7
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-03 14:12:22 +0900

    display of all products in product#index & selected products in homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :about, :cart ]
 
   def home
+    @products = Product.where(rating: 4).or(Product.where(rating: 5))
   end
 
   def about
