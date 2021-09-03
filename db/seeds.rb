71312a8e97d0af318378485857e59ca8905198a3
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-04 08:54:04 +0900

    finished set up for cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,16 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require "faker"
+require "open-uri"
+
+
+puts "Deleting all Test reviews"
+
+Review.delete_all
 
 puts "Deleting all Fake products"
 
-Product.destroy_all
+Product.delete_all
 
 puts "Deleting Admin user"
 
@@ -24,15 +30,25 @@ puts "Admin user created!"
 
 puts "Creating database of Fake products"
 
-30.times do
+file = URI.open('https://cdn.nogarlicnoonions.com/images/thumbs/image.php?width=2000&height=2000&image=/images/articles/2021-07/best-coffee-beans-luxe-digital2021-07-11-10-19-37.jpg')
+file1 = URI.open('https://www.gardeniapharmacy.com/wp-content/uploads/2021/02/coffee.jpg')
+file2 = URI.open('https://blog.warriorcoffee.com/hubfs/_MG_3386.jpg')
+
+files = [file, file1, file2]
+
+1.times do
     product = Product.new(
         name: Faker::Tea.variety, 
         description: Faker::Lorem.sentence(word_count: 20), 
         price: rand(2000...10000), 
         rating: rand(1..5), 
+        availability: rand(1..2)
     )
     product.user = admin
 
+    files.each do |file|
+        product.photos.attach(io: file, filename: 'nes.jpg', content_type: 'image/jpg')
+    end
     product.save!
 end
 
