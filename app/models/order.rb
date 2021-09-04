46727568dc8a30129f291ddc1d1a91d8957fec57
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-05 07:51:14 +0900

    added validations to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, dependent: :destroy
+
+  validates :status, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 2 }
+  validates :date, presence: true
+  validates :subtotal, presence: true, numericality: { only_integer: true }
+  validates :total, presence: true, numericality: { only_integer: true }
 end
