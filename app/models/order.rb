b1b66cadb234cfb769d1345acb1c2b1a1646c3a9
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-05 08:03:08 +0900

    writting callbacks for interaction between order_item & order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,7 +3,10 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
 
   validates :status, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 2 }
+  #status 1 => open // status 2 => completed
   validates :date, presence: true
+  #the date at first will be the creation date but it has to change to the date-time where we did the CHECKOUT
   validates :subtotal, presence: true, numericality: { only_integer: true }
   validates :total, presence: true, numericality: { only_integer: true }
+  #this we will go with callbacks --> before creating we check 
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,18 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  before_validation :generate_order
+
+  private
+
+  def generate_order
+    if Order.find_by(status: 1)
+      
+    else
+      order = Order.new(status: 1)
+      order.user = current_user
+      order.save!
+    end
+  end
 end
