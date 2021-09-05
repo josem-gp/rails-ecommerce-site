c61d8a4d3af6749658bcb495e1030be0a61335ba
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-05 19:39:18 +0900

    fixed bug to show only orders once they have been completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     def show
         @user = current_user
         @favorites = @user.all_favorited
-        @orders = @user.orders
+        @orders = @user.orders.where(status: 2)
     end
 
     def edit
