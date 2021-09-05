c64e31fcef60de2853aabb649c9594aad24a802f
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-05 18:58:10 +0900

    checkout logic done & cart rendering conditionally

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,10 @@ class OrdersController < ApplicationController
             redirect_to root_path, notice: "Product not ordered, please try again"
         end
     end
+
+    def update
+        @order = Order.find(params[:id])
+        @order.update(status: 2)
+        redirect_to root_path, notice: "Thank you for your order!"
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     def show
         @user = current_user
         @favorites = @user.all_favorited
+        @orders = @user.orders
     end
 
     def edit
