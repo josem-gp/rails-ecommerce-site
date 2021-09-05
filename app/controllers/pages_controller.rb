c0faa66264b39101aa58ab5a19189a81ace5b47c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-05 16:30:19 +0900

    conditional rendering of cart page if empty or if it has items

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,7 +11,7 @@ class PagesController < ApplicationController
   def cart
     if current_user
       @order = Order.where(user: current_user, status: 1)[0]
-      @cart_items = @order.order_items
+      @cart_items = @order.order_items if @order
     else
       redirect_to new_user_session_path
     end
