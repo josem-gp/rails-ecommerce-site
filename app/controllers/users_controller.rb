cab60d9605074b51e3decb81687cf1b57fae1859
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-05 19:01:07 +0900

    check logic behind favorites for rendering in user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
     def show
         @user = current_user
         @favorites = @user.all_favorited
+        #check this maybe change for all_favorites
         @orders = @user.orders
     end
 
