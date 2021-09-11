7d714792e44aaaf1dba1a3bfc09f3546fc0b9166
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-11 14:33:14 +0900

    edited orders controller for shipment

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,12 +10,13 @@ class OrdersController < ApplicationController
     end
 
     def update
+        @user = current_user
         @order = Order.find(params[:id])
         t = Time.now()
         time = t.strftime("%Y-%m-%d %H:%M:%S")
         @order.update(status: 2, date: time)
         if @order.update
-            ShippingConfirmationMailer.with(user: current_user, order: @order).shipping_confirmation_email.deliver_later
+            ShippingConfirmationMailer.with(user: @user, order: @order).shipping_confirmation_email.deliver_later
             redirect_to root_path, notice: "Thank you for your order!"
         else
             redirect_to root_path, notice: "Product not ordered, please try again"
