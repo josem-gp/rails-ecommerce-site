8d49c584ab91d784faa4dcafb642498008c207cb
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-11 13:12:17 +0900

    filled ShippingConfirmation mailer model

--- app/mailers/shipping_confirmation_mailer.rb
+++ app/mailers/shipping_confirmation_mailer.rb
@@ -1,2 +1,10 @@
 class ShippingConfirmationMailer < ApplicationMailer
+    default from: 'myfakestore@email.com'
+
+    def shipping_confirmation_email
+        @user = params[:user]
+        @order = params[:order]
+
+        mail(to: @user.email, subject: "Your shipment is on it's way")
+    end
 end
