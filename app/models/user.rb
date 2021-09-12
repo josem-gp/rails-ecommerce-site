8421f2e977827f2bf6bad8192cbfe0b60cd52343
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-12 18:09:29 +0900

    added callback to User model for welcome message after registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,11 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, uniqueness: { case_sensitive: false }, length: { minimum: 3 }
   validates :email, presence: true, uniqueness: true
 
+  after_create :send_registration_email
+
+  private
+
+  def send_registration_email
+    UserConfirmationMailer.with(user: self).user_registration_email.deliver_later
+  end
 end
