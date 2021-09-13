99cef69688856fa996dfe44d00d5d88733ea9dae
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-14 05:42:34 +0900

    added logic to product model for pg search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ApplicationRecord
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_description,
+    against: [ :name, :description ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   belongs_to :user
   has_many :reviews, dependent: :destroy
   has_many :order_items, dependent: :destroy
