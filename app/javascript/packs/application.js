d8d6300ef02ed6da57afb90081872d57e3abf892
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-17 20:49:37 +0900

    created logic for stars in rating form

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,8 +34,38 @@ document.addEventListener("turbolinks:load", () => {
   const stars = document.querySelectorAll("input[type='radio']");
 
   stars.forEach((star) => {
-    star.addEventListener("onmouseover", (event) => {
-      console.log(event.target);
+    star.addEventListener("mouseover", (event) => {
+      const filteredStars = Array.from(stars).filter(
+        (star) => parseInt(star.value, 10) <= parseInt(event.target.value, 10)
+      );
+
+      filteredStars.forEach((filtered) => {
+        filtered.parentElement.classList.add("full-star");
+      });
+    });
+
+    star.addEventListener("mouseleave", (event) => {
+      const filteredStars = Array.from(stars).filter(
+        (star) => parseInt(star.value, 10) <= parseInt(event.target.value, 10)
+      );
+
+      filteredStars.forEach((filtered) => {
+        filtered.parentElement.classList.remove("full-star");
+      });
+    });
+
+    star.addEventListener("click", (event) => {
+      stars.forEach((star) => {
+        star.parentElement.classList.remove("active");
+      });
+
+      const filteredStars = Array.from(stars).filter(
+        (star) => parseInt(star.value, 10) <= parseInt(event.target.value, 10)
+      );
+
+      filteredStars.forEach((filtered) => {
+        filtered.parentElement.classList.add("active");
+      });
     });
   });
 });
