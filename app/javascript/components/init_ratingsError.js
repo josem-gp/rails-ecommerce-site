0ad94e98760af3d58476c9563212b860bf7c0d6c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-18 21:40:21 +0900

    done logic but needs improvement

--- app/javascript/components/init_ratingsError.js
+++ app/javascript/components/init_ratingsError.js
@@ -2,46 +2,83 @@ const ratingsError = () => {
   const ratings = document.querySelectorAll(".fa-star");
   const submitRating = document.querySelector('input[value="Create Review"]');
   const ratingsDiv = document.querySelector(".review_rating");
-  const ratingsLabel =
-    document.querySelector(".review_rating").firstElementChild;
   const titleDiv = document.querySelector('input[name="review[title]"');
   const contenDiv = document.querySelector('textarea[name="review[content]"');
-  const titleWrapper = document.querySelector("#title");
-  const contentWrapper = document.querySelector("#content");
 
   submitRating.addEventListener("click", (event) => {
     console.log("clicked!");
+    ratingsDiv.classList.remove("invalid-stars");
+    titleDiv.classList.remove("invalid-title");
+    contenDiv.classList.remove("invalid-content");
 
-    if (ratings[0].classList.contains("active")) {
+    if (
+      ratings[0].classList.contains("active") &&
+      titleDiv.value &&
+      contenDiv.value
+    ) {
       console.log("free!");
-      ratingsDiv.classList.remove("invalid-stars");
-      titleDiv.classList.remove("invalid-title");
-      contenDiv.classList.remove("invalid-content");
-      titleWrapper.classList.remove("invalid-title-div");
-      contentWrapper.classList.remove("invalid-content-div");
-    } else {
-      event.preventDefault();
-      console.log("doomed!");
-      if (titleDiv.value && contenDiv.value) {
+    } else if (
+      !ratings[0].classList.contains("active") ||
+      !titleDiv.value ||
+      !contenDiv.value
+    ) {
+      if (
+        !ratings[0].classList.contains("active") &&
+        !titleDiv.value &&
+        !contenDiv.value
+      ) {
+        titleDiv.classList.add("invalid-title");
+        contenDiv.classList.add("invalid-content");
+        ratingsDiv.classList.add("invalid-stars");
+      } else if (
+        !ratings[0].classList.contains("active") &&
+        titleDiv.value &&
+        contenDiv.value
+      ) {
         titleDiv.classList.add("valid-title");
         contenDiv.classList.add("valid-content");
-      } else if (titleDiv.value && !contenDiv.value) {
+        ratingsDiv.classList.add("invalid-stars");
+      } else if (
+        !ratings[0].classList.contains("active") &&
+        titleDiv.value &&
+        !contenDiv.value
+      ) {
         titleDiv.classList.add("valid-title");
-        contentWrapper.classList.remove("invalid-title-div");
         contenDiv.classList.add("invalid-content");
-        contentWrapper.classList.add("invalid-content-div");
-      } else if (!titleDiv.value && contenDiv.value) {
+        ratingsDiv.classList.add("invalid-stars");
+      } else if (
+        !ratings[0].classList.contains("active") &&
+        !titleDiv.value &&
+        contenDiv.value
+      ) {
         titleDiv.classList.add("invalid-title");
-        titleWrapper.classList.add("invalid-title-div");
         contenDiv.classList.add("valid-content");
-        contentWrapper.classList.remove("invalid-content-div");
-      } else if (!titleDiv.value && !contenDiv.value) {
+        ratingsDiv.classList.add("invalid-stars");
+      } else if (
+        ratings[0].classList.contains("active") &&
+        !titleDiv.value &&
+        !contenDiv.value
+      ) {
         titleDiv.classList.add("invalid-title");
-        titleWrapper.classList.add("invalid-title-div");
         contenDiv.classList.add("invalid-content");
-        contentWrapper.classList.add("invalid-content-div");
+        ratingsDiv.classList.add("valid-stars");
+      } else if (
+        ratings[0].classList.contains("active") &&
+        titleDiv.value &&
+        !contenDiv.value
+      ) {
+        titleDiv.classList.add("valid-title");
+        contenDiv.classList.add("invalid-content");
+        ratingsDiv.classList.add("valid-stars");
+      } else if (
+        ratings[0].classList.contains("active") &&
+        !titleDiv.value &&
+        contenDiv.value
+      ) {
+        titleDiv.classList.add("invalid-title");
+        contenDiv.classList.add("valid-content");
+        ratingsDiv.classList.add("valid-stars");
       }
-      ratingsDiv.classList.add("invalid-stars");
     }
   });
 };
