9ebfc7072716fac173972b52d2d6e3b3ef06d85c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-18 16:19:37 +0900

    fixed bug & changed stars to 1 in the beginning

--- app/javascript/components/init_fiveStars.js
+++ app/javascript/components/init_fiveStars.js
@@ -1,7 +1,8 @@
 //this is as a default because if not it creates an error if the form is sent without stars
 
 const fiveStars = () => {
-  document.querySelector("#review_rating_5").click();
+  const fiveStars = document.querySelector("#review_rating_1");
+  fiveStars ? fiveStars.click() : "";
 };
 
 export { fiveStars };
