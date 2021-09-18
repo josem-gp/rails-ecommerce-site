baea10cd15ab0eeffe63f5d177ca35299706ffe8
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-18 16:43:50 +0900

    fixed bug for form without having to click a star in the beginning

--- app/javascript/components/init_fiveStars.js
+++
@@ -1,8 +0,0 @@
-//this is as a default because if not it creates an error if the form is sent without stars
-
-const fiveStars = () => {
-  const fiveStars = document.querySelector("#review_rating_1");
-  fiveStars ? fiveStars.click() : "";
-};
-
-export { fiveStars };
