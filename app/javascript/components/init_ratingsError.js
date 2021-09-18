5cd9f04bb40ee443e78025bc94b5656d36dc7489
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-19 07:29:52 +0900

    fixed some bugs

--- app/javascript/components/init_ratingsError.js
+++ app/javascript/components/init_ratingsError.js
@@ -3,89 +3,89 @@ const ratingsError = () => {
   const submitRating = document.querySelector('input[value="Create Review"]');
   const ratingsDiv = document.querySelector(".review_rating");
   const titleDiv = document.querySelector('input[name="review[title]"');
-  const contenDiv = document.querySelector('textarea[name="review[content]"');
+  const contentDiv = document.querySelector('textarea[name="review[content]"');
   if (submitRating) {
     submitRating.addEventListener("click", (event) => {
       console.log("clicked!");
       ratingsDiv.classList.remove("invalid-stars");
       titleDiv.classList.remove("invalid-title");
-      contenDiv.classList.remove("invalid-content");
+      contentDiv.classList.remove("invalid-content");
 
       if (
         ratings[0].classList.contains("active") &&
         titleDiv.value &&
-        contenDiv.value
+        contentDiv.value
       ) {
         console.log("free!");
       } else if (
         !ratings[0].classList.contains("active") ||
         !titleDiv.value ||
-        !contenDiv.value
+        !contentDiv.value
       ) {
         event.preventDefault();
         console.log("doomed!");
         if (
           !ratings[0].classList.contains("active") &&
           !titleDiv.value &&
-          !contenDiv.value
+          !contentDiv.value
         ) {
           console.log("one!");
           titleDiv.classList.add("invalid-title");
-          contenDiv.classList.add("invalid-content");
+          contentDiv.classList.add("invalid-content");
           ratingsDiv.classList.add("invalid-stars");
         } else if (
           !ratings[0].classList.contains("active") &&
           titleDiv.value &&
-          contenDiv.value
+          contentDiv.value
         ) {
           console.log("two!");
           titleDiv.classList.add("valid-title");
-          contenDiv.classList.add("valid-content");
+          contentDiv.classList.add("valid-content");
           ratingsDiv.classList.add("invalid-stars");
         } else if (
           !ratings[0].classList.contains("active") &&
           titleDiv.value &&
-          !contenDiv.value
+          !contentDiv.value
         ) {
           console.log("three!");
           titleDiv.classList.add("valid-title");
-          contenDiv.classList.add("invalid-content");
+          contentDiv.classList.add("invalid-content");
           ratingsDiv.classList.add("invalid-stars");
         } else if (
           !ratings[0].classList.contains("active") &&
           !titleDiv.value &&
-          contenDiv.value
+          contentDiv.value
         ) {
           console.log("four!");
           titleDiv.classList.add("invalid-title");
-          contenDiv.classList.add("valid-content");
+          contentDiv.classList.add("valid-content");
           ratingsDiv.classList.add("invalid-stars");
         } else if (
           ratings[0].classList.contains("active") &&
           !titleDiv.value &&
-          !contenDiv.value
+          !contentDiv.value
         ) {
           console.log("five!");
           titleDiv.classList.add("invalid-title");
-          contenDiv.classList.add("invalid-content");
+          contentDiv.classList.add("invalid-content");
           ratingsDiv.classList.add("valid-stars");
         } else if (
           ratings[0].classList.contains("active") &&
           titleDiv.value &&
-          !contenDiv.value
+          !contentDiv.value
         ) {
           console.log("six!");
           titleDiv.classList.add("valid-title");
-          contenDiv.classList.add("invalid-content");
+          contentDiv.classList.add("invalid-content");
           ratingsDiv.classList.add("valid-stars");
         } else if (
           ratings[0].classList.contains("active") &&
           !titleDiv.value &&
-          contenDiv.value
+          contentDiv.value
         ) {
           console.log("seven!");
           titleDiv.classList.add("invalid-title");
-          contenDiv.classList.add("valid-content");
+          contentDiv.classList.add("valid-content");
           ratingsDiv.classList.add("valid-stars");
         }
       }
