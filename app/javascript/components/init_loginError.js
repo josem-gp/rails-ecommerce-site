063c48091160a67b29a94de36202d317286a464a
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-19 11:43:57 +0900

    added text when error review form

--- app/javascript/components/init_loginError.js
+++ app/javascript/components/init_loginError.js
@@ -2,16 +2,17 @@ const loginError = () => {
   const emailDiv = document.querySelector('input[name="user[email]"');
   const passwordDiv = document.querySelector('input[name="user[password]"');
   const checkboxDiv = document.querySelector("#user_remember_me");
-
-  if (
-    emailDiv.classList.contains("is-valid") ||
-    passwordDiv.classList.contains("is-valid")
-  ) {
-    emailDiv.classList.remove("is-valid");
-    emailDiv.classList.add("invalid-title");
-    passwordDiv.classList.remove("is-valid");
-    passwordDiv.classList.add("invalid-title");
-    checkboxDiv.classList.remove("is-valid");
+  if (emailDiv && passwordDiv && checkboxDiv) {
+    if (
+      emailDiv.classList.contains("is-valid") ||
+      passwordDiv.classList.contains("is-valid")
+    ) {
+      emailDiv.classList.remove("is-valid");
+      emailDiv.classList.add("invalid-neutral");
+      passwordDiv.classList.remove("is-valid");
+      passwordDiv.classList.add("invalid-neutral");
+      checkboxDiv.classList.remove("is-valid");
+    }
   }
 };
 
