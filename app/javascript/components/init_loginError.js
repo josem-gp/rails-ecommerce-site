00663bb662f112cbd9e9e3c6ca8da030742e1864
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-19 12:08:44 +0900

    fixed logic in loginError

--- app/javascript/components/init_loginError.js
+++ app/javascript/components/init_loginError.js
@@ -2,14 +2,14 @@ const loginError = () => {
   const emailDiv = document.querySelector('input[name="user[email]"');
   const passwordDiv = document.querySelector('input[name="user[password]"');
   const checkboxDiv = document.querySelector("#user_remember_me");
-  if (emailDiv && passwordDiv && checkboxDiv) {
-    if (
-      emailDiv.classList.contains("is-valid") ||
-      passwordDiv.classList.contains("is-valid")
-    ) {
+  const errorTag = document.querySelector(".alert-warning");
+
+  if (errorTag) {
+    console.log("yes!");
+    if (emailDiv && passwordDiv && checkboxDiv) {
+      console.log("we here!");
       emailDiv.classList.remove("is-valid");
       emailDiv.classList.add("invalid-neutral");
-      passwordDiv.classList.remove("is-valid");
       passwordDiv.classList.add("invalid-neutral");
       checkboxDiv.classList.remove("is-valid");
     }
