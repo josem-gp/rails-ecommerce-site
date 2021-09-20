e71fd75dc8d942fad9241a599bf09993d40fff76
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-21 05:32:05 +0900

    added round to logic for average

--- app/models/review.rb
+++ app/models/review.rb
@@ -20,7 +20,7 @@ class Review < ApplicationRecord
       total += review.rating
     end
 
-    review_average = total/product.reviews.count
+    review_average = (total/product.reviews.count).round(1)
 
     product.update({rating: review_average})
 
