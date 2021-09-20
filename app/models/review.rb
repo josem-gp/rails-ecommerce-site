ab6e986b026134b85753b3325b335a74fc9d3079
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-21 05:30:24 +0900

    added logic for review_average each time a review is added to a product

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,4 +6,24 @@ class Review < ApplicationRecord
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
   validates :content, presence: true, length: { in: 5..250 }
 
+  after_save :add_rating_to_product 
+
+  private
+
+  def add_rating_to_product
+    rating = self.rating
+    product = self.product
+
+    total = 0
+
+    product.reviews.each do |review| 
+      total += review.rating
+    end
+
+    review_average = total/product.reviews.count
+
+    product.update({rating: review_average})
+
+  end
+
 end
