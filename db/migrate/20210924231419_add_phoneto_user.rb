598e34f1abeb0d7defc9fad8fad1a3ec9fb1e492
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-25 08:16:01 +0900

    added phone number for user info when shipping

--- db/migrate/20210924231419_add_phoneto_user.rb
+++ db/migrate/20210924231419_add_phoneto_user.rb
@@ -1,5 +1,5 @@
 class AddPhonetoUser < ActiveRecord::Migration[6.0]
   def change
-    
+    add_column :users, :phone_number, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_220259) do
+ActiveRecord::Schema.define(version: 2021_09_24_231419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_220259) do
     t.boolean "admin"
     t.string "name"
     t.string "shipping_address"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["icon_id"], name: "index_users_on_icon_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
