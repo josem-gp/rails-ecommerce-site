e1db73bf94fe1b15c503d36150427907470871ac
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-09-26 09:43:36 +0900

    add unavailable tag to product if availability == 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -29,6 +29,7 @@ class Product < ApplicationRecord
   validates :name, presence: true, uniqueness: true, uniqueness: { case_sensitive: false }
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
+  # Availability: 1 available / 2 unavailable
   validates :availability, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 2 }
 
 end
