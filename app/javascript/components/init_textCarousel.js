ec73085f434b2f1d6c8effb1753c09839214260b
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-02 09:43:39 +0900

    finished js for caption carousel

--- app/javascript/components/init_textCarousel.js
+++ app/javascript/components/init_textCarousel.js
@@ -1,6 +1,20 @@
 const textCarousel = () => {
   const carousel = document.querySelector(".carousel-inner");
-  const carouselTitle = document.querySelector(".carousel-inner");
+  const carouselTitle = document.querySelector(".carousel-caption h1");
+  const carouselSubtitle = document.querySelector(".carousel-caption p");
+  const carouselBtns = document.querySelector(".cta-btns");
+
+  if (carousel) {
+    setTimeout(function () {
+      carouselTitle.style.visibility = "visible";
+    }, 2000);
+    setTimeout(function () {
+      carouselSubtitle.style.visibility = "visible";
+    }, 2500);
+    setTimeout(function () {
+      carouselBtns.style.visibility = "visible";
+    }, 3000);
+  }
 };
 
 export { textCarousel };
