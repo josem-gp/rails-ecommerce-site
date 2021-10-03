1c516cbbaea2e50ff1dbba2daee70e5bc5f9bb5b
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-03 09:28:48 +0900

    deleted js for typed, put script on the html

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -37,7 +37,6 @@ import { disableStripe } from "../components/init_disableStripe";
 import { toggleSearch } from "../components/init_toggleSearch";
 import { dropdownNav } from "../components/init_dropdownNav";
 import { originalNav } from "../components/init_originalNav";
-import { typedStrings } from "../components/init_typedStrings";
 
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
@@ -53,5 +52,4 @@ document.addEventListener("turbolinks:load", () => {
   toggleSearch();
   dropdownNav();
   originalNav();
-  // typedStrings();
 });
