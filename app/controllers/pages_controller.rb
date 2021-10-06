9df48435de237f0e599c6fb4929d9a7b0b4beb45
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-07 08:16:37 +0900

    changed redirect contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about, :new, :create ]
-
-  def new
-    @contact = Contact.new()
-  end
+  skip_before_action :authenticate_user!, only: [ :home, :about, :create ]
 
   def create
     @contact = Contact.new(params[:contact])
@@ -15,16 +11,11 @@ class PagesController < ApplicationController
         @contact = Contact.new()
         flash[:alert] = "Thank you for your message. We will get back to you soon!"
         render :home
-        
-        # format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
       else
         @user = current_user
         @products = Product.all
-        @contact = Contact.new()
         flash[:alert] = "Message did not send."
         render :home
-        
-        # format.js   { flash.now[:error] = @message = "Message did not send." }
       end
   end
 
