f2195910ea7044ef6cbba391f043605316bc328d
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-11 17:38:23 +0900

    changed controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
     skip_before_action :authenticate_user!, only: [ :index, :show ]
-#  before_action :controller_name, only: [:show ]
 
     def index
         @user = current_user
@@ -49,17 +48,5 @@ class ProductsController < ApplicationController
 
     def product_params
         params.require(:product).permit(:name, :description, :price, :availability, photos: [])
-    end
-
-    def controller_name
-        raise
-        # the idea is to get params[:id] and if it is a name then check if it is capitalized or uppercased and if thats the case redirect to lowercase path
-        #to check the path we can use request.path. Also keep in mind that sometimes the params[:id] is downcase but whats before has the uppercase so we can just downcase the whole url?
-        # ['2Arfasd4', 'Computology', 'app', '123', "A2dadf4"].map { |el| /[[:digit:]]/.match(el) ? "yes"  : "no" } => ["yes", "no", "no", "yes", "yes"]
-        # request.path.split("/").map { |el| /[[:digit:]]/.match(el) ? el  : el.downcase }.join("/") => "/products/138"
-        unless request.path.split("/").select{|el| el.match(/^[[:upper:]][a-zA-Z]+/)}.blank?
-            redirect_to request.path.split("/").map { |el| /[[:digit:]]/.match(el) ? el  :  el == "" ? "" : el.downcase}.join("/"), :status => :moved_permanently
-        end
-
     end
 end
