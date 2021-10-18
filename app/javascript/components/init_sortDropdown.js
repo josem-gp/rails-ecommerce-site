68b9af32233d9c173af3837079924ecc18c7cd3c
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-18 21:19:46 +0900

    fixed bugs here and there

--- app/javascript/components/init_sortDropdown.js
+++ app/javascript/components/init_sortDropdown.js
@@ -2,11 +2,13 @@ const sortDropdown = () => {
   const dropdownBtn = document.querySelector(".sort_drop");
   const dropdownMenu = document.querySelector(".made-dropdown-menu");
 
-  dropdownBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    dropdownBtn.firstElementChild.classList.toggle("upwards");
-    dropdownMenu.classList.toggle("show");
-  });
+  if (dropdownBtn) {
+    dropdownBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      dropdownBtn.firstElementChild.classList.toggle("upwards");
+      dropdownMenu.classList.toggle("show");
+    });
+  }
 };
 
 export { sortDropdown };
