9598c72f310e81ffc2240626e2e98cd8c3aea7de
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-20 07:04:20 +0900

    created newsletter params to implement on controller

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -1,2 +1,10 @@
 class NewslettersController < ApplicationController
+    def create
+        raise
+    end
+
+    private
+    def newsletter_params
+        params.require(:newsletter).permit(:email)
+    end
 end
