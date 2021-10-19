b09e4098c9bec63d52adff7d30086ed4d6813819
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-20 08:27:29 +0900

    created logic for controller and redirects

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -3,10 +3,16 @@ class NewslettersController < ApplicationController
 
     def create
         raise
+        @newsletter = Newsletter.new(newsletter_params)
+        if @newsletter.save!
+            redirect_to request.referer, notice: "Thank you for subscribing to our newsletter!"
+        else 
+            redirect_to request.referer, notice: "There was an error in your form"
+        end
     end
 
     private
     def newsletter_params
-        params.require(:newsletter).permit(:email)
+        params.permit(:email)
     end
 end
