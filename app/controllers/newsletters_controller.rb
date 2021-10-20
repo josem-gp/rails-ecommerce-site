b902aea549db47bf9d4ba7302a832a372fe4b86e
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-20 19:19:26 +0900

    done a minimal regex validation for email

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -2,11 +2,15 @@ class NewslettersController < ApplicationController
     skip_before_action :authenticate_user!, only: [ :create ]
 
     def create
-        @newsletter = Newsletter.new(newsletter_params)
-        authorize @newsletter
-        if @newsletter.save
-            redirect_to request.referer, notice: "Thank you for subscribing to our newsletter!"
-        else 
+        if newsletter_params[:email].match(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/)
+            @newsletter = Newsletter.new(newsletter_params)
+            authorize @newsletter
+            if @newsletter.save
+                redirect_to request.referer, notice: "Thank you for subscribing to our newsletter!"
+            else 
+                redirect_to request.referer, notice: "There was an error in your form"
+            end
+        else
             redirect_to request.referer, notice: "There was an error in your form"
         end
     end
