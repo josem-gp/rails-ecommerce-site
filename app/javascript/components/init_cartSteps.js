62abd0f4b50fc9c5b5b0699061c949ab21b1f70b
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-10-24 16:16:17 +0900

    succesfully added logic behind transition in steps cart

--- app/javascript/components/init_cartSteps.js
+++ app/javascript/components/init_cartSteps.js
@@ -3,6 +3,11 @@ const cartSteps = () => {
   const editShippingDiv = document.querySelector(".change-shipping-info");
   const changeBilling = document.querySelector(".change-billing-btn");
   const editBillingDiv = document.querySelector(".change-billing-info");
+  const goStepOneBtn = document.querySelector(".go-step-one");
+  const goStepTwoBtn = document.querySelector(".go-step-two");
+  const goStepThreeBtn = document.querySelector(".go-step-three");
+  const formDivs = document.querySelectorAll(".form-step");
+  const stepBtnArray = [goStepOneBtn, goStepTwoBtn, goStepThreeBtn];
 
   if (changeShipping) {
     changeShipping.addEventListener("click", (event) => {
@@ -12,6 +17,24 @@ const cartSteps = () => {
   if (changeBilling) {
     changeBilling.addEventListener("click", (event) => {
       editBillingDiv.classList.toggle("form-height-adjusted");
+    });
+  }
+  if (goStepOneBtn || goStepTwoBtn || goStepThreeBtn) {
+    stepBtnArray.forEach((btn) => {
+      btn.addEventListener("click", (ev) => {
+        console.log(ev.target);
+        formDivs.forEach((form) => {
+          if (ev.target.dataset.key === form.dataset.step) {
+            form.classList.contains("form-hidden")
+              ? form.classList.remove("form-hidden")
+              : "";
+          } else {
+            form.classList.contains("form-hidden")
+              ? ""
+              : form.classList.add("form-hidden");
+          }
+        });
+      });
     });
   }
 };
