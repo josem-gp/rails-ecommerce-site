c9920c828890ae47da6072444003547c485087ea
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-11-06 09:39:46 +0900

    finished main html for user registration email

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -27,7 +27,6 @@ class PagesController < ApplicationController
   end
 
   def about
-    raise
     @user = current_user
   end
 end
--- app/mailers/user_confirmation_mailer.rb
+++ app/mailers/user_confirmation_mailer.rb
@@ -2,6 +2,7 @@ class UserConfirmationMailer < ApplicationMailer
 
     def user_registration_email
         @user = params[:user]
+        @featured_products =Product.order(sales: :desc).first(3)
 
         mail(to: @user.email, subject: "Welcome to #{ENV['SHOP_NAME']}")
     end
