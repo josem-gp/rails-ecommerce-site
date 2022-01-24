ae292492ec81112f0883e78e7bd48c6ddbd8fef4
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-01-24 14:57:23 +0900

    factory bot created for review

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,8 +1,39 @@
 FactoryBot.define do
   factory :review do
-    content { "MyText" }
-    rating { 1 }
-    user { nil }
-    product { nil }
+    trait :with_title do
+      title { "Review_title_test" }
+    end
+
+    trait :with_correct_content do
+      content { "Lorem ipsum dolor sit amet" }
+    end
+
+    trait :with_incorrect_content do
+      content { "Lo" }
+    end
+
+    trait :with_correct_rating do
+      username { 5 }
+    end
+
+    trait :with_incorrect_rating do
+      username { 6 }
+    end
+
+    association :user, factory: :non_admin_user
+    association :product, factory: :correct_product
+
+    factory :non_title_review, traits: [:with_correct_content, :with_correct_rating]
+    factory :non_content_review, traits: [:with_title, :with_correct_rating]
+    factory :non_rating_review, traits: [:with_title, :with_correct_content]
+    factory :incorrect_review_content, traits: [:with_title, :with_incorrect_content, :with_correct_rating]
+    factory :incorrect_review_rating, traits: [:with_title, :with_correct_content, :with_incorrect_rating]
+    factory :correct_review, traits: [:with_title, :with_correct_content, :with_correct_rating]
   end
 end
+
+# Review needs to have title
+# Review needs to have content
+# Review needs to have content between 3 and 250 char
+# Review needs to have rating
+# Review needs to have rating between 1 and 5
