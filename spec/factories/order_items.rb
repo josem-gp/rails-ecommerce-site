ae76f9b0152d751de2ad6da967c70e262ce9f174
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-01-24 14:06:41 +0900

    review factory bot for user

--- spec/factories/order_items.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :order_item do
-    order { nil }
-    product { nil }
-  end
-end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,18 @@
 FactoryBot.define do
   factory :user do
-    username { "jose_test" }
-    sequence(:email) { |n| "jose_test#{n}@test.io" }
     password { "1234567" }
     name { "Jose Test" }
     shipping_address { "jose_shipping_test" }
     phone_number { "08012341234" }
     association :icon
+
+    trait :with_username do
+      username { "jose_test" }
+    end
+
+    trait :with_email do
+      sequence(:email) { |n| "jose_test#{n}@test.io" }
+    end
 
     trait :admin do
       admin { true }
@@ -16,14 +22,19 @@ FactoryBot.define do
       admin { false }
     end
 
-    factory :admin_user, traits: [:admin]
-    factory :non_admin_user, traits: [:not_admin]
+    factory :non_email_user, traits: [:with_username, :not_admin]
+    factory :non_username_user, traits: [:with_email, :not_admin]
+    factory :admin_user, traits: [:with_username, :with_email,:admin]
+    factory :non_admin_user, traits: [:with_username, :with_email, :not_admin]
   end
 end
 
-# User needs to have unique email
+
+# User needs to have username
 # User needs to have unique username (case sensitive - false)
 # User needs to have username with minimun length of 3
+# User needs to have email
+# User needs to have unique email
 # User gets icon before validation
 # User is added to newsletter after creation
 # User gets a confirmation email after creation
