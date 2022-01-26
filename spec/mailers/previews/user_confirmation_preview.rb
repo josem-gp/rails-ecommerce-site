17c3e2e0a107bf690fbda5db7a1a02db150dddbd
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-01-26 18:15:04 +0900

    added preview for email

--- spec/mailers/previews/user_confirmation_preview.rb
+++ spec/mailers/previews/user_confirmation_preview.rb
@@ -1,4 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_confirmation
 class UserConfirmationPreview < ActionMailer::Preview
+    def user_registration_email
+        user = User.new(username: "saki_test", email: "saki@hotmail.com", password: "1234567")
 
+        UserConfirmationMailer.with(user: user).user_registration_email
+    end
 end
