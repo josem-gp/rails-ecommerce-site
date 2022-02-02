be3bcaddd6b01bd4b7a1cae847f6fcfb9d21cb9d
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-02-02 09:27:14 +0900

    specs finished for shipping confirmation mailer

--- spec/mailers/shipping_confirmation_spec.rb
+++ spec/mailers/shipping_confirmation_spec.rb
@@ -1,5 +1,17 @@
 require "rails_helper"
 
 RSpec.describe ShippingConfirmationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notify" do
+    let(:correct_order) { FactoryBot.create(:correct_order) }
+    let(:mail) { ShippingConfirmationMailer.with(user: correct_order.user, order: correct_order).shipping_confirmation_email }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Your shipment is on it's way")
+      expect(mail.to).to eq([correct_order.user.email])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Shipping confirmation")
+    end
+  end
 end
--- spec/mailers/user_confirmation_spec.rb
+++ spec/mailers/user_confirmation_spec.rb
@@ -2,11 +2,12 @@ require "rails_helper"
 
 RSpec.describe UserConfirmationMailer, type: :mailer do
   describe "notify" do
-    let(:mail) { UserConfirmationMailer.with(user: FactoryBot.create(:non_admin_user)).user_registration_email }
+    let(:correct_user) { FactoryBot.create(:non_admin_user) }
+    let(:mail) { UserConfirmationMailer.with(user: correct_user).user_registration_email }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Welcome to ")
-      expect(mail.to).to eq(["jose_test1@test.io"])
+      expect(mail.to).to eq([correct_user.email])
     end
 
     it "renders the body" do
