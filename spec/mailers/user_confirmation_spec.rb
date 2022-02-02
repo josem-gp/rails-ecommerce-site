f2718ab8273b2c2c9f151ceea24d4795514c4325
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-02-02 09:21:05 +0900

    created spec for user_confirmation_mailer

--- spec/mailers/user_confirmation_spec.rb
+++ spec/mailers/user_confirmation_spec.rb
@@ -1,5 +1,16 @@
 require "rails_helper"
 
 RSpec.describe UserConfirmationMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "notify" do
+    let(:mail) { UserConfirmationMailer.with(user: FactoryBot.create(:non_admin_user)).user_registration_email }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Welcome to ")
+      expect(mail.to).to eq(["jose_test1@test.io"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Welcome!")
+    end
+  end
 end
