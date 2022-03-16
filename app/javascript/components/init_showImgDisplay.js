5d736d0eb2f7f9178d83d46080dc1c695572d025
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-03-16 21:36:24 +0900

    done animation on transition for images on product page

--- app/javascript/components/init_showImgDisplay.js
+++ app/javascript/components/init_showImgDisplay.js
@@ -1,3 +1,16 @@
+function FadeOut(target, duplicate, speed) {
+  var seconds = speed / 1000;
+  target.firstElementChild.style.transition = "opacity " + seconds + "s ease";
+  target.firstElementChild.style.position = "absolute";
+
+  target.firstElementChild.style.opacity = 0;
+  target.insertAdjacentHTML("beforeend", duplicate);
+  target.getElementsByTagName("img")[1].classList.add("augment-pic");
+  setTimeout(function () {
+    target.firstElementChild.remove();
+  }, speed);
+}
+
 const showImgDisplay = () => {
   const displayDiv = document.querySelector(".main-pic");
   if (displayDiv) {
@@ -9,11 +22,12 @@ const showImgDisplay = () => {
 
     productImgs.forEach((img) => {
       img.addEventListener("click", (event) => {
-        let duplicate = event.target.outerHTML;
+        if (displayDiv.childElementCount === 1) {
+          //if we try to click images too quickly, it will break things so we make sure it only has 1 child
+          let duplicate = event.target.outerHTML;
 
-        displayDiv.firstElementChild.remove();
-        displayDiv.insertAdjacentHTML("beforeend", duplicate);
-        displayDiv.firstElementChild.classList.add("augment-pic");
+          FadeOut(displayDiv, duplicate, 500); //with this we do a FadeOut animation when passing each image
+        }
       });
     });
   }
