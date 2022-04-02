69719dd6cc1cbaa0d3366e64597f752fa44cf383
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-04-02 13:56:32 +0900

    finished template, design and tested shipping

--- app/mailers/shipping_confirmation_mailer.rb
+++ app/mailers/shipping_confirmation_mailer.rb
@@ -4,6 +4,7 @@ class ShippingConfirmationMailer < ApplicationMailer
     def shipping_confirmation_email
         @user = params[:user]
         @order = params[:order]
+        @order_items = OrderItem.where(order_id: @order.id)
 
         mail(to: @user.email, subject: "Your shipment is on it's way")
     end
