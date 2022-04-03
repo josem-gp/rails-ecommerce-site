0ae31d9f2f6f6f2e8a33b8bb553e0fa800de9e49
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-04-03 20:40:05 +0900

    working new stripe API v2

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,7 +1,5 @@
 class ChargesController < ApplicationController
   # before_action :authenticate_user!
-  before_action :set_description
-  
 
   def new
     @user = current_user
@@ -22,28 +20,26 @@ class ChargesController < ApplicationController
 
   def create
     @amount = Order.where(user: current_user, status: 1)[0].total
-    customer = StripeTool.create_customer(email: params[:stripeEmail], 
-                                          stripe_token: params[:stripeToken])
+    session = Stripe::Checkout::Session.create({
+      line_items: [{
+        # Provide the exact Price ID (e.g. pr_1234) of the product you want to sell
+        price: "price_1KkR7ZGsOm5WfgBcyWZX3I2v",
+        quantity: 1,
+      }],
+      mode: 'payment',
+      success_url: ENV['DOMAIN'] + '/success.html',
+      cancel_url: ENV['DOMAIN'] + '/cancel.html',
+    })
 
-    charge = StripeTool.create_charge(customer_id: customer.id, 
-                                      amount: @amount,
-                                      description: @description)
-
+    redirect_to session.url
+    #Create another route called success which is connected to the success_url
     orders_controller = OrdersController.new
     @user = current_user
     @order = Order.where(user: current_user, status: 1)[0]
     authorize @order
-    orders_controller.update(@user, @order)
-    redirect_to root_path, notice: "Your order has been made!"
-
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+    orders_controller.update(@user, @order)  
+   else
+     
+   end
   end
-
-  private
-
-    def set_description
-      @description = "Purchase in #{ENV['SHOP_NAME']}"
-    end
 end
