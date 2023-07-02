78a21cfb9f34a7d31ce891c5e9841fc732eb488c
Jose Manuel Garcia Portillo <69992326+josem-gp@users.noreply.github.com>
2023-07-02 15:07:11 +0900

    feat: udpated pagination, typed and contact form (#3)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,8 +6,8 @@ class Contact < MailForm::Base
     { 
       #this is the subject for the email generated, it can be anything you want
       subject: "My Contact Form",
-      to: %("#{name}" <#{email}>),
-      from: ENV['GMAIL_SMTP_USER']
+      to: ENV['GMAIL_SMTP_USER'],
+      from: %("#{name}" <#{email}>)
       #the from will display the name entered by the user followed by the email
     }
   end
